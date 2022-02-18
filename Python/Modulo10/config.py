8decdac8d70a85b33fa52f69ed920f4629132767
miguelrohdez <nomeacuerdox4@gmail.com>
2022-02-17 20:26:35 -0600

    Termino de Kata 10

--- Python/Modulo10/config.py
+++ Python/Modulo10/config.py
@@ -1,9 +1,46 @@
+'''
 def main():
     try:
         configuration = open('config.txt')
     except FileNotFoundError:
         print("Couldn't find the config.txt file!")
+'''
 
+'''
+def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")
+'''
+'''
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+'''
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+
+'''
+try:
+     open("mars.jpg")
+ except FileNotFoundError as err:
+     print("got a problem trying to read the file:", err)
+
+got a problem trying to read the file: [Errno 2] No such file or directory: 'mars.jpg'
+'''
 
 if __name__ == '__main__':
     main()
