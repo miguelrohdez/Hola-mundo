03445d4e1e28e0297dfbc5e65014697a808c4fb2
miguelrohdez <nomeacuerdox4@gmail.com>
2022-03-07 23:00:18 -0600

    JS

---
+++ Frontend/JavaScript/Clases/intro.js
@@ -0,0 +1,15 @@
+console.log("\n ************** Variables **************"); 
+var numero1;
+numero1 = 4;
+var numero2;
+numero2 = 2;
+console.log(numero1);
+
+console.log("\n ************** Cadenas **************"); 
+var frase1;
+frase1 = "Ejemplo comillas dobles";
+var frase2;
+frase2 = 'Ejemplo comillas simples';
+var frase3;
+frase3 = `Ejemplo comillas simples`;
+console.log(frase1);
