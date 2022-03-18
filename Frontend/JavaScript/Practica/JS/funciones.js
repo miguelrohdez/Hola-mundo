d83bd467f88321f4c4d7a0ba699ec92e09bdcc42
miguelrohdez <nomeacuerdox4@gmail.com>
2022-03-18 00:51:48 -0600

    Quitando lo que no sirve

--- Frontend/JavaScript/Practica/JS/funciones.js
+++ Frontend/JavaScript/Practica/JS/funciones.js
@@ -1,5 +1,3 @@
-var lang = 'es';
-
 const buscarPokemon = () => {
     const url = "https://pokeapi.co/api/v2/pokemon/torchic";
     fetch(url).then((res) =>{
@@ -11,14 +9,10 @@ const buscarPokemon = () => {
         }
     }).then((data) => {
         console.log(data);
-        console.log(data.types[0].type.name);
-        let tipo = data.types[0].type.name;
-        console.log(tipo);
-        console.log(getNameTipo(tipo));
         
         const pokeDatos = {
             img: data.sprites.other['official-artwork'].front_default,
-            tipo: getNameTipo(data.types[0].type.name),
+            tipo: data.types[0].type.name,
             estadisticas: {
                 hp: data.stats[0].base_stat,
                 ataque: data.stats[1].base_stat,
@@ -26,38 +20,32 @@ const buscarPokemon = () => {
                 ataqueEspecial: data.stats[3].base_stat,
                 defensaEspecial: data.stats[4].base_stat,
                 velocidad: data.stats[5].base_stat
-            },
-        
-        
-        
-        
+            }
+            
         };
         
         console.log(pokeDatos);
-        //const pokeMoves = data.moves.map(x => getNameMove(x.move.name));
         
-        
-
     });
-
+    
 }
 
+
+function getNameTipo(tipo){
+    fetch(`https://pokeapi.co/api/v2/type/${tipo}`)
+    .then((res) => res.json())
+    .then(dataType =>{
+        salida = dataType.names[5].name;
+    }); 
+        
+}
 
 
 function getNameMove(name){
     let moves = [];
     fetch(`https://pokeapi.co/api/v2/move/${name}`)
     .then((res) => res.json())
-    .then((dataMove) => moves.push(dataMove.names[5].name));
+    .then(dataMove => moves.push(dataMove.names[5].name));
     console.log(moves);
-}
+}    
 
-function getNameTipo(tipo){
-    let tipoNombre;
-    fetch(`https://pokeapi.co/api/v2/type/${tipo}`)
-    .then((res) => res.json())
-    .then((dataType) => tipoNombre = dataType.names[5].name)
-    return tipoNombre;
-}
-
-buscarPokemon();
