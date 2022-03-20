2cf802fc0cadf7976651c2a81af18b725c34d839
miguelrohdez <nomeacuerdox4@gmail.com>
2022-03-19 21:31:20 -0600

    Marca Ubndefined

--- Frontend/JavaScript/Practica/JS/funciones.js
+++ Frontend/JavaScript/Practica/JS/funciones.js
@@ -1,5 +1,8 @@
 const buscarPokemon = () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/torchic";
+    const pokeNameInput = document.getElementById("pokemon-findName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     fetch(url).then((res) =>{
         if (res.status != "200") {
             //pokeImage("./pokemon-sad.gif")
@@ -8,28 +11,60 @@ const buscarPokemon = () => {
             return res.json();
         }
     }).then((data) => {
-        console.log(data);
-        
-        const pokeDatos = {
-            img: data.sprites.other['official-artwork'].front_default,
-            tipo: data.types[0].type.name,
-            estadisticas: {
-                hp: data.stats[0].base_stat,
-                ataque: data.stats[1].base_stat,
-                defensa: data.stats[2].base_stat,
-                ataqueEspecial: data.stats[3].base_stat,
-                defensaEspecial: data.stats[4].base_stat,
-                velocidad: data.stats[5].base_stat
-            }
+        if (data) {
+            console.log(data)
             
-        };
-        
-        console.log(pokeDatos);
-        
-    });
-    
+            const pokeDatos = {
+                nombre: data.name, 
+                img: data.sprites.other['official-artwork'].front_default,
+                tipo: data.types[0].type.name,
+                estadisticas: {
+                    hp: data.stats[0].base_stat,
+                    ataque: data.stats[1].base_stat,
+                    defensa: data.stats[2].base_stat,
+                    ataqueEspecial: data.stats[3].base_stat,
+                    defensaEspecial: data.stats[4].base_stat,
+                    velocidad: data.stats[5].base_stat
+                },
+                
+            }
+            let pokeImg = data.types[0].type.name
+            console.log(pokeDatos)
+            pokeNombre(pokeDatos.name)
+            pokeImagen(pokeDatos.img)
+            pokeTipo(pokeDatos)
+        }
+    })
 }
 
+
+const pokeNombre = (u) => {
+    const pokeName = document.getElementById("pokemon-name");
+    pokeName.innerText = u;
+    console.log(pokeName)
+}
+
+const pokeImagen = (url) => {
+    const pokePhoto = document.getElementById("pokemon-img");
+    pokePhoto.src = url;
+}
+
+
+const pokeTipo = (url) => {
+    const pokeType = document.getElementById("pokemon-tipo");
+    pokeType.src = url;
+}
+
+
+const pokeEstadisticas = (url) => {
+    const pokeStats = document.getElementById("pokemon-stats");
+    pokeStats.src = url;
+}
+
+const pokeMovimientos = (url) => {
+    const pokeMoves = document.getElementById("pokemon-moves");
+    pokeMoves.src = url;
+}
 
 function getNameTipo(tipo){
     fetch(`https://pokeapi.co/api/v2/type/${tipo}`)
@@ -48,4 +83,3 @@ function getNameMove(name){
     .then(dataMove => moves.push(dataMove.names[5].name));
     console.log(moves);
 }    
-
